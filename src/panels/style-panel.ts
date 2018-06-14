81c129b1dbbbdf02c313004b2cae6b989b575384
Francois Chabbey <chabbey78@gmail.com>
2018-06-14 09:23:37 -0700

    Empty style-panel

---
+++ src/panels/style-panel.ts
@@ -0,0 +1,8 @@
+import { html } from 'lit-html/lib/lit-extended.js';
+import { LitElement } from '@polymer/lit-element/lit-element.js';
+
+//TODO find a colorpicker for style and stroke
+
+export class StylePanel extends LitElement {
+
+}
